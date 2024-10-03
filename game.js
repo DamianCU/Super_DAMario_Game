bc733f71e1d17fd5dae4a34a8f5862c780bee993
DamianCU <158050732+DamianCU@users.noreply.github.com>
2024-10-03 18:52:08 +0200

    Super DAMario Game

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ const config= {
     type: Phaser.AUTO, // webgl, canvas
     width: 256,
     height: 244,
-    backgroundColor: "white",
+    backgroundColor: "#049cd8",
     parent: 'game',
     scene: {
         preload, // se ejecutará para precargar recursos
@@ -17,12 +17,33 @@ new Phaser.Game(config)
 function preload() {
     this.load.image(
         'cloud1', 'assets/scenery/overworld/cloud1.png'
+    )
+    this.load.image(
+        'floorbricks',
+        'assets/scenery/overworld/floorbricks.png'
+    )
+    this.load.spritesheet(
+        'mario',
+        'assets/entities/mario.png',
+        {frameWidth: 18, frameHeight:16}
     )  
 }
 
 function create() {
-    this.add.image(0,0,'cloud1')
+    // image(x, y, id-del-aset)
+    this.add.image(100,50,'cloud1')
+        .setOrigin(0.5,0.5)
+        .setScale(0.15)
+    
+    this.add.tileSprite(0, config.height - 16,config.width, 32, 'floorbricks')
+        .setOrigin(0, 0.5)
+        
+
+    this.add.sprite(50,210, 'mario')
+        .setOrigin(0,1)
+
 }
+
 
 function update() {
     console.log('update');    
